b9da1e6b15e27f95399a8643a5a8db64bde449a9
skelmis <skelmis.craft@gmail.com>
2022-02-19 03:18:57 +1300

    Update admin header

--- nextcord_stats/urls.py
+++ nextcord_stats/urls.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
 
-admin.site.site_title = "Nextcord Statistics"
+admin.site.site_title = "Nextcord Statistics"  # The stuff shown in tabs
 admin.site.site_header = "Nextcord Statistics Admin Page"
 
 urlpatterns = [
